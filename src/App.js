adcbdf943ec3dac8e5c050cea4a1c6f0441c22e3
GopalaKrishnan.R <gopal.krishnan@gulaq.com>
2020-01-02 14:25:52 +0530

    TAG20200102-01 :  Inital commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  const add = () => {
+    setCount(count + 1);
+  };
+
+  const subract = () => {
+    setCount(count <= 0 ? 0 : count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <p>SSR Example Calculations</p>
+      <p>Sum of Value : {count}</p>
+      <button onClick={add}>+ Add</button>
+      <button onClick={subract}>- Subract</button>
+    </React.Fragment>
   );
 }
 
